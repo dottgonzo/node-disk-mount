f3862fb8e1614299f2ba972e190536aa65986d57
Dario Caruso <dcaruso@movia.biz>
2018-03-23 11:36:34 +0100

    add bitlocker mount

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Promise = require("bluebird");
 const lsdisks = require("ls-disks");
 const child_process = require("child_process");
+const pathExists = require("path-exists");
 function checkpart(part) {
     return new Promise((resolve, reject) => {
         const partitions = lsdisks.listPartitions();
@@ -39,6 +40,66 @@ function mount(part, dir) {
     });
 }
 exports.mount = mount;
+function mountLockedWithBitLocker(part, dir, dislockerDir) {
+    return new Promise((resolve, reject) => {
+        checkpart(part).then((parti) => {
+            if (!dislockerDir && pathExists.sync('/mnt/dislocker'))
+                dislockerDir = '/mnt/dislocker';
+            if (!dislockerDir)
+                return reject('no dislocker folder found');
+            if (!pathExists.sync(dislockerDir))
+                child_process.execSync('sudo mkdir -p ' + dislockerDir + ' && sudo chmod 777 ' + dislockerDir);
+            if (!pathExists.sync(dir))
+                child_process.execSync('sudo mkdir -p ' + dir + ' && sudo chmod 777 ' + dir);
+            child_process.exec("sudo dislocker -V " + parti.partition + " -ubitlocker -- " + dislockerDir, (err, stdout, stderr) => {
+                if (err) {
+                    reject(err);
+                }
+                else {
+                    child_process.exec("sudo mount  -o loop,rw " + dislockerDir + "/dislocker-file " + dir, (err, stdout, stderr) => {
+                        if (err) {
+                            reject(err);
+                        }
+                        else {
+                            resolve(true);
+                        }
+                    });
+                }
+            });
+        }).catch((err) => {
+            reject(err);
+        });
+    });
+}
+exports.mountLockedWithBitLocker = mountLockedWithBitLocker;
+function umountLockedWithBitLocker(dir, dislockerDir) {
+    return new Promise((resolve, reject) => {
+        if (!dislockerDir && pathExists.sync('/mnt/dislocker'))
+            dislockerDir = '/mnt/dislocker';
+        if (!dislockerDir)
+            return reject('no dislocker folder found');
+        if (!pathExists.sync(dir))
+            return reject('no mount point finded to umount');
+        if (!pathExists.sync(dislockerDir))
+            return reject('no dislocker finded to umount');
+        child_process.exec("sudo umount " + dir, (err, stdout, stderr) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                child_process.exec("sudo umount " + dislockerDir, (err, stdout, stderr) => {
+                    if (err) {
+                        reject(err);
+                    }
+                    else {
+                        resolve(true);
+                    }
+                });
+            }
+        });
+    });
+}
+exports.umountLockedWithBitLocker = umountLockedWithBitLocker;
 function umount(dirOrPart) {
     return new Promise((resolve, reject) => {
         child_process.exec("cat /etc/mtab | grep -c '" + dirOrPart + "'", (err, stdout, stderr) => {
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import * as Promise from "bluebird";
 import * as lsdisks from "ls-disks";
 import * as child_process from "child_process";
+import * as pathExists from "path-exists";
 
 
 
@@ -50,6 +51,89 @@ export function mount(part: string, dir: string): Promise<true> {
   })
 
 }
+
+
+export function mountLockedWithBitLocker(part: string, dir: string, dislockerDir?: string): Promise<true> {
+  // manca il controllo del se già è montato
+  // sarebbe in oltre possibile montare la partizione senza specificare la directory qualora la partizione esiste sull'fstab
+  return new Promise<true>((resolve, reject) => {
+
+    checkpart(part).then((parti) => {
+
+
+      if (!dislockerDir && pathExists.sync('/mnt/dislocker')) dislockerDir = '/mnt/dislocker'
+
+      if (!dislockerDir) return reject('no dislocker folder found')
+
+      if (!pathExists.sync(dislockerDir)) child_process.execSync('sudo mkdir -p '+dislockerDir+' && sudo chmod 777 '+dislockerDir)
+
+      if (!pathExists.sync(dir)) child_process.execSync('sudo mkdir -p '+dir+' && sudo chmod 777 '+dir)
+
+
+      // dislocker -V /dev/sdc1 -ubitlocker -- /mnt/dislocker/
+
+      // mount -o loop,rw /mnt/dislocker/dislocker-file /bitlocker/
+
+      child_process.exec("sudo dislocker -V " + parti.partition + " -ubitlocker -- " + dislockerDir, (err, stdout, stderr) => {
+        if (err) {
+          reject(err)
+        } else {
+          child_process.exec("sudo mount  -o loop,rw " + dislockerDir + "/dislocker-file " + dir, (err, stdout, stderr) => {
+            if (err) {
+              reject(err)
+            } else {
+              resolve(true)
+            }
+          })
+        }
+      })
+    }).catch((err) => {
+      reject(err)
+    })
+
+  })
+
+}
+
+
+export function umountLockedWithBitLocker(dir: string, dislockerDir?: string): Promise<true> {
+  // manca il controllo del se già è montato
+  // sarebbe in oltre possibile montare la partizione senza specificare la directory qualora la partizione esiste sull'fstab
+  return new Promise<true>((resolve, reject) => {
+
+
+    if (!dislockerDir && pathExists.sync('/mnt/dislocker')) dislockerDir = '/mnt/dislocker'
+
+    if (!dislockerDir) return reject('no dislocker folder found')
+
+    if (!pathExists.sync(dir)) return reject('no mount point finded to umount')
+    if (!pathExists.sync(dislockerDir)) return reject('no dislocker finded to umount')
+
+    // #umount
+    // umount /bitlocker
+    // umount /mnt/dislocker
+
+    child_process.exec("sudo umount " + dir, (err, stdout, stderr) => {
+      if (err) {
+        reject(err)
+      } else {
+        child_process.exec("sudo umount " + dislockerDir, (err, stdout, stderr) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(true)
+          }
+        })
+      }
+    })
+
+
+  })
+
+}
+
+
+
 
 export function umount(dirOrPart: string): Promise<true> {
   // il controllo del mountpoint è impreciso
